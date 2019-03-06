8c53dc2198f2c79e1e75977430527570d13fe133
Giovanni <gifrancischelli@gmail.com>
2019-03-05 21:38:51 -0300

    :lipstick: Add basic Theming and configure styled-components types

---
+++ components/Theme.tsx
@@ -0,0 +1,56 @@
+import reset from "styled-reset";
+import { createGlobalStyle, ThemeProps } from "styled-components";
+import baseStyled, {
+  ThemedStyledInterface,
+  ThemeProvider as BaseThemeProvider,
+  ThemeProviderComponent
+} from "styled-components";
+
+const colors = {
+  primary: "#DB306F",
+  background: "#FAFAFA",
+  grey: {
+    light: "#7D7B7B",
+    neutral: "#6E6366",
+    dark: "#57494C"
+  }
+};
+
+const margins = {
+  sm: "8px",
+  md: "16px",
+  lg: "32px"
+};
+
+export const theme = {
+  colors,
+  margins
+};
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Cabin:400,600,700');
+
+  ${reset}
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  html, body {
+
+    background-color: ${p => p.theme.colors.background};
+
+    font-family: 'Cabin', sans-serif !important;
+    font-weight: 400;
+    font-style: normal;
+    color: ${(p: ThemeProps<Theme>) => p.theme.colors.grey.light};
+  }
+
+  p {
+    line-height: 1.6;
+  }
+`;
+
+export type Theme = typeof theme;
+export const styled = baseStyled as ThemedStyledInterface<Theme>;
+export const ThemeProvider = BaseThemeProvider as ThemeProviderComponent<Theme>;
