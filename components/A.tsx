e4c9d2848e9b94dc31b6a7bf29efc0b766b6ec62
Giovanni <gifrancischelli@gmail.com>
2019-03-21 17:07:00 -0300

    :lipstick: Add hover animation do link tags

--- components/A.tsx
+++ components/A.tsx
@@ -1,9 +1,30 @@
 import { styled } from "./Theme";
 
 const A = styled.a`
-  cursor: pointer;
-  text-decoration: underline;
+  position: relative;
   color: ${p => p.theme.colors.primary};
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: calc(100% + 1px);
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: ${p => p.theme.colors.primary};
+    transition: transform 0.2s ease-in-out;
+    transform-origin: left;
+    transform: scaleX(0);
+  }
+
+  &:hover:before {
+    transform: scaleX(1);
+    transform-origin: right;
+  }
 `;
 
 export default A;
