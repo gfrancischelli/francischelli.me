c59fc7c46aa14e2b2eec3c6bde4fafc59f841977
Giovanni <gifrancischelli@gmail.com>
2019-03-24 20:32:39 -0300

    :lipstick: Change A hover animation to a more reliable one

--- components/A.tsx
+++ components/A.tsx
@@ -3,27 +3,11 @@ import { styled } from "./Theme";
 const A = styled.a`
   position: relative;
   color: ${p => p.theme.colors.primary};
+  transition: background-color 0.15s ease-in-out;
 
   &:hover {
     cursor: pointer;
-  }
-
-  &:before {
-    content: "";
-    position: absolute;
-    top: calc(100% + 1px);
-    left: 0;
-    width: 100%;
-    height: 2px;
-    background-color: ${p => p.theme.colors.primary};
-    transition: transform 0.2s ease-in-out;
-    transform-origin: left;
-    transform: scaleX(0);
-  }
-
-  &:hover:before {
-    transform: scaleX(1);
-    transform-origin: right;
+    background-color: ${p => p.theme.colors.pink.light};
   }
 `;
 
