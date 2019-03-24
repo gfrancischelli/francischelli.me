8e09412a6d6af5a1eceb4b62690b7c8eb248776c
Giovanni <gifrancischelli@gmail.com>
2019-03-24 20:52:38 -0300

    :lipstick: Improve Posts styling using Text components and add Meta

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,12 +1,18 @@
 import { styled } from "./Theme";
 import { PostMeta } from "../content/posts";
-// import { default as Link } from "next/link";
+import { default as Link } from "next/link";
+import Meta from "./Meta";
+import A from "./A";
+import { H3 } from "./Text";
 
 type Props = {
   data: PostMeta[];
 };
 
 const Wrapper = styled.ul`
+  margin-top: ${p => p.theme.margins.md};
+  font-family: ${p => p.theme.fonts.accent};
+
   h3 {
     font-size: 22px;
     color: ${p => p.theme.colors.pink};
@@ -16,21 +22,24 @@ const Wrapper = styled.ul`
     margin-top: ${p => p.theme.margins.lg};
   }
 
-  #length {
-    font-size: "14px";
-    color: ${p => p.theme.colors.grey.lighter};
+  .meta {
     margin-top: ${p => p.theme.margins.md};
   }
 `;
 
 const Posts: React.FC<Props> = ({ data }) => (
   <Wrapper>
-    {data.map(({ title, path, length }) => (
-      <li>
-        <a href={path}>
-          <h3>{title}</h3>
-        </a>
-        <div id="length">{length} min read</div>
+    {data.map(({ title, path, ...meta }) => (
+      <li key={path}>
+        <Link href={path}>
+          <H3>
+            <A>{title}</A>
+          </H3>
+        </Link>
+
+        <div className="meta">
+          <Meta {...meta} />
+        </div>
       </li>
     ))}
   </Wrapper>
