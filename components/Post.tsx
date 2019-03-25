b15a8f2692052a4ae18d84d2da52b7b9c3aba99f
Giovanni <gifrancischelli@gmail.com>
2019-03-24 21:04:40 -0300

    :sparkles: Add Post component

---
+++ components/Post.tsx
@@ -0,0 +1,31 @@
+import { PostMeta } from "../content/posts";
+import { styled } from "../components/Theme";
+import { H1 } from "../components/Text";
+import Meta from "../components/Meta";
+
+const Wrapper = styled.div`
+  .meta {
+    margin-top: ${p => p.theme.margins.md};
+    margin-bottom: ${p => p.theme.margins.lg};
+    font-family: ${p => p.theme.fonts.accent};
+    font-size: 14px;
+    font-weight: medium;
+    color: ${p => p.theme.colors.base.main};
+  }
+`;
+
+type Props = {
+  meta: PostMeta;
+};
+
+const Post: React.FC<Props> = ({ children, meta }) => (
+  <Wrapper>
+    <H1>{meta.title}</H1>
+    <div className="meta">
+      <Meta {...meta} />
+    </div>
+    <div id="content">{children}</div>
+  </Wrapper>
+);
+
+export default Post;
